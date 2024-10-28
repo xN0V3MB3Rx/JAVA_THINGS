853ab59a8e1733f87a311202b54e000fdf29868e
N0V3MB3R.M1K3 <n0v3mb3r.m1k3.2dot0@gmail.com>
2024-10-28 16:14:05 -0300

    Deleted codes

--- Example3.java
+++
@@ -1,9 +0,0 @@
-class Example3{
-    public static void main(String args[]){
-        int iNum1 = 100;
-        for(int iCont = 1; iCont <= iNum1; ++iCont){
-            System.out.println("Step: " + iCont);
-        }
-        System.out.println("Conteo finalizado.");
-    }
-}
--- TRAINING/Review1.java
+++
@@ -1,7 +0,0 @@
-package TRAINING;
-
-class Review1 {
-    static void main(String args[]){
-        System.out.println("Hello world, this is JAVA.");
-    }
-}   
